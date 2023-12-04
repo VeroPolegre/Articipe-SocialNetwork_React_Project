a7a5502b6ff8ee1711be7da74a30254b301b66f6
VeroPolegre <veronicapolegre98@gmail.com>
2023-12-04 14:20:18 +0100

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./fonts.scss";
 import SignUp from "./pages/SignUp/SignUp";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
       </Router>
