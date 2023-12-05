c17102d522f4b1f0682df19c68574385f6979144
VeroPolegre <veronicapolegre98@gmail.com>
2023-12-05 12:39:44 +0100

    register component form data implemented

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,73 +4,36 @@ import { useDispatch } from "react-redux";
 import { register } from "../../features/auth/authSlice";
 
 const Register = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    dob: "",
-    avatar: "",
-    password: "",
-    password2: "",
-  });
-
-  const { username, email, dob, avatar, password, password2 } = formData;
   const dispatch = useDispatch();
-
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    if (e.target.avatar.files[0])
+      formData.set("avatar", e.target.avatar.files[0]);
+    formData.set("username", e.target.username.value);
+    formData.set("email", e.target.email.value);
+    formData.set("dob", e.target.dob.value);
+    formData.set("password", e.target.password.value);
+    formData.set("password2", e.target.password2.value);
     dispatch(register(formData));
   };
 
   return (
     <>
       <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={onChange}
-          placeholder="username"
-        />
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={onChange}
-          placeholder="email"
-        />
-        <input
-          type="date"
-          name="dob"
-          value={dob}
-          onChange={onChange}
-          placeholder="date of birth"
-        />
+        <input type="text" name="username" placeholder="username" />
+        <input type="email" name="email" placeholder="email" />
+        <input type="date" name="dob" placeholder="date of birth" />
         <input
           type="file"
           name="avatar"
-          value={avatar}
-          onChange={onChange}
           accept="image/png, image/jpeg, image/jpg"
         />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={onChange}
-          placeholder="password"
-        />
+        <input type="password" name="password" placeholder="password" />
         <input
           type="password"
           name="password2"
-          value={password2}
-          onChange={onChange}
           placeholder="confirm password"
         />
         <button type="submit">Register now</button>
