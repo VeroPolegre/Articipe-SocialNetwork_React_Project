1ab677f41c2bdcdda149331f46b8a95c033e56a5
Frances Morales Velilla <frances0688@gmail.com>
2023-12-07 17:44:03 +0100

    Remove redundant isError=false since adding reset

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,65 +4,63 @@ const user = JSON.parse(localStorage.getItem("user"));
 const token = JSON.parse(localStorage.getItem("token"));
 
 const initialState = {
-  user: user || null,
-  token: token || null,
-  isError: false,
-  isSuccess: false,
-  message: "",
+	user: user || null,
+	token: token || null,
+	isError: false,
+	isSuccess: false,
+	message: "",
 };
 
 export const register = createAsyncThunk(
-  "auth/register",
-  async (formData, thunkAPI) => {
-    try {
-      return await authService.register(formData);
-    } catch (error) {
-      console.error(error);
-      const message = error.response.data.message;
-      console.log(message);
-      return thunkAPI.rejectWithValue(message);
-    }
-  }
+	"auth/register",
+	async (formData, thunkAPI) => {
+		try {
+			return await authService.register(formData);
+		} catch (error) {
+			console.error(error);
+			const message = error.response.data.message;
+			console.log(message);
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
 );
 
 export const login = createAsyncThunk("auth/login", async (user) => {
-  try {
-    return await authService.login(user);
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		return await authService.login(user);
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 export const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    reset: (state) => {
-      state.isError = false;
-      state.isSuccess = false;
-      state.message = "";
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(register.fulfilled, (state, action) => {
-        state.isSuccess = true;
-        state.isError = false;
-        state.message = action.payload.message;
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-      })
-      .addCase(register.rejected, (state, action) => {
-        state.isError = true;
-        state.isSuccess = false;
-        state.message = action.payload;
-      })
-      .addCase(login.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.isError = false;
-      });
-  },
+	name: "auth",
+	initialState,
+	reducers: {
+		reset: (state) => {
+			state.isError = false;
+			state.isSuccess = false;
+			state.message = "";
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(register.fulfilled, (state, action) => {
+				state.isSuccess = true;
+				state.message = action.payload.message;
+				state.user = action.payload.user;
+				state.token = action.payload.token;
+			})
+			.addCase(register.rejected, (state, action) => {
+				state.isError = true;
+				state.message = action.payload;
+			})
+			.addCase(login.fulfilled, (state, action) => {
+				state.isSuccess = true;
+				state.user = action.payload.user;
+				state.token = action.payload.token;
+			});
+	},
 });
 
 export default authSlice.reducer;
