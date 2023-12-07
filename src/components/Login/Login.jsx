79f01198e25689c7fb232b3c9f691cd0b8d9078d
Cocera <alcocera@gmail.com>
2023-12-07 12:14:40 +0100

    style login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { login } from "../../features/auth/authSlice";
 import { useDispatch } from "react-redux";
+import "./Login.scss"
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -52,35 +53,42 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
+    <form onSubmit={onSubmit} className="form-register">
+      <div className="custom-label-input">
+        <label htmlFor="usernameFormLogin" className="material-symbols-outlined">person</label>
         <input
           type="text"
           name="username"
           placeholder="username"
           value={username}
           onChange={onChange}
+          id="usernameFormLogin"
         />
         {errors.username && <p>{errors.username}</p>}
       </div>
 
-      <div>
+      <div className="custom-label-input">
+        <label htmlFor="passwordFormLogin" className="material-symbols-outlined">lock</label>
         <input
           type="password"
           name="password"
           placeholder="password"
           value={password}
           onChange={onChange}
+          id="passwordFormLogin"
         />
         {errors.password && <p>{errors.password}</p>}
       </div>
 
       <button type="submit">Login</button>
+
       {loginError && <p>{loginError}</p>}
-      <div>
-        Don't have an account yet?
+
+      <p>
+        Don't have an account yet?<br></br>
         <a href="../register">Create one now.</a>
-      </div>
+      </p>
+      
     </form>
   );
 };
