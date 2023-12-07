c51af38b7f4978a8e621f5da798c2e5148c21206
Luciana Boasso <luboasso16@gmail.com>
2023-12-07 13:32:47 +0100

    Add links to MainNav

--- src/components/Header/MainNav/MainNav.jsx
+++ src/components/Header/MainNav/MainNav.jsx
@@ -1,19 +1,30 @@
+import { Link } from "react-router-dom";
 import "./MainNav.scss";
 
 const MainNav = () => {
-    return (
-        <>
-            <nav className="main-nav">
-                <ul>
-                    <li className="material-symbols-outlined">home</li>
-                    <li className="material-symbols-outlined">search</li>
-                    <li className="material-symbols-outlined">add_box</li>
-                    <li className="material-symbols-outlined">inbox</li>
-                    <li className="material-symbols-outlined">account_circle</li>
-                </ul>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav className="main-nav">
+        <ul>
+          <Link to="/">
+            <li className="material-symbols-outlined">home</li>
+          </Link>
+          <Link to="/search">
+            <li className="material-symbols-outlined">search</li>
+          </Link>
+          <Link to="/add">
+            <li className="material-symbols-outlined">add_box</li>
+          </Link>
+          <Link to="/inbox">
+            <li className="material-symbols-outlined">inbox</li>
+          </Link>
+          <Link to="/home">
+            <li className="material-symbols-outlined">account_circle</li>
+          </Link>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default MainNav;
