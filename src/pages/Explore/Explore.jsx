3950daccf8087e036b18d1460a58f1c71b4c4512
Cocera <alcocera@gmail.com>
2023-12-13 12:27:34 +0100

    PostGrid component edited

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "../../components/Header/header";
 import PostGrid from "../../components/PostGrid/PostGrid";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import { useSelector } from "react-redux";
 
 const Explore = () => {
+
+	const { posts } = useSelector((state) => state.posts);
+
 	return (
 		<React.Fragment>
 			<Header />
 			<SearchBar />
-			<PostGrid />
+			<PostGrid posts={posts}/>
 		</React.Fragment>
 	);
 };
