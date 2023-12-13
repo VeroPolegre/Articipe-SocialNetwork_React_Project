a4f9c306f8801efa953380e903c3eaad6a328d19
VeroPolegre <veronicapolegre98@gmail.com>
2023-12-13 13:05:27 +0100

    getUsersByName in search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,44 +2,47 @@ import { useState } from "react";
 import "./SearchBar.scss";
 import { useDispatch } from "react-redux";
 import { getPostsByKeywords } from "../../features/posts/postsSlice";
+import { getUsersByName } from "../../features/user/userSlice";
 
 const SearchBar = () => {
-	const [keywords, setKeywords] = useState("");
-	const dispatch = useDispatch();
+  const [keywords, setKeywords] = useState("");
+  const dispatch = useDispatch();
 
-	const handleChange = (e) => {
-		setKeywords(e.target.value);
-	};
+  const handleChange = (e) => {
+    setKeywords(e.target.value);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const keywordsArray = keywords.split(", ").map((keyword) => keyword.trim());
-		dispatch(getPostsByKeywords(keywordsArray));
-		setKeywords("");
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const keywordsArray = keywords.split(", ").map((keyword) => keyword.trim());
+    dispatch(getPostsByKeywords(keywordsArray));
+    dispatch(getUsersByName(keywordsArray));
+    setKeywords("");
+  };
 
-	return (
-		<>
-			<form className="searchBarNav" onSubmit={handleSubmit}>
-				<div className="custom-label-input">
-					<label
-						htmlFor="searchInput"
-						className="material-symbols-outlined label-icon">
-						search
-					</label>
-					<input
-						type="text"
-						name="keywords"
-						value={keywords}
-						onChange={handleChange}
-						placeholder="Search"
-						id="searchInput"
-						className="text-input"
-					/>
-				</div>
-			</form>
-		</>
-	);
+  return (
+    <>
+      <form className="searchBarNav" onSubmit={handleSubmit}>
+        <div className="custom-label-input">
+          <label
+            htmlFor="searchInput"
+            className="material-symbols-outlined label-icon"
+          >
+            search
+          </label>
+          <input
+            type="text"
+            name="keywords"
+            value={keywords}
+            onChange={handleChange}
+            placeholder="Search"
+            id="searchInput"
+            className="text-input"
+          />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default SearchBar;
--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header/header";
 import PostGrid from "../../components/PostGrid/PostGrid";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import UserGrid from "../../components/UserGrid/UserGrid";
 
 const Explore = () => {
-	return (
-		<React.Fragment>
-			<Header />
-			<SearchBar />
-			<PostGrid />
-		</React.Fragment>
-	);
+  const [searchType, setSearchType] = useState("post");
+  const handleSearchTypeChange = (type) => {
+    setSearchType(type);
+  };
+
+  return (
+    <React.Fragment>
+      <Header />
+      <SearchBar onSearchTypeChange={handleSearchTypeChange} />
+      <main className="explore-main">
+        {searchType === "post" && <PostGrid />}
+        {searchType === "user" && <UserGrid />}
+      </main>
+    </React.Fragment>
+  );
 };
 
 export default Explore;
