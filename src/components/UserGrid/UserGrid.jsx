4db7d57d5cb4e2a083f5e8654ae2608e8c3f5e9b
Frances Morales Velilla <frances0688@gmail.com>
2023-12-14 15:29:44 +0100

    Create follow/unfollow buttons, service, and slice.

--- src/components/UserGrid/UserGrid.jsx
+++ src/components/UserGrid/UserGrid.jsx
@@ -1,8 +1,32 @@
-import { List, Avatar } from "antd";
+import { List, Avatar, Button } from "antd";
 import "./UserGrid.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { follow, unfollow } from "../../features/user/userSlice";
 
 const UserGrid = ({ users }) => {
-	console.log("users:", users);
+	const dispatch = useDispatch();
+	const followingUser = useSelector((state) => state.user.user.following);
+
+	const isFollowing = (userId) => {
+		return followingUser.some((user) => user._id === userId);
+	};
+
+	const handleFollow = async (userId) => {
+		try {
+			await dispatch(follow(userId));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleUnfollow = async (userId) => {
+		try {
+			await dispatch(unfollow(userId));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	if (!users || users.length === 0) {
 		return <div>No users found</div>;
 	} else {
@@ -22,6 +46,11 @@ const UserGrid = ({ users }) => {
 							}
 							title={user.username}
 						/>
+						{isFollowing(user._id) ? (
+							<Button onClick={() => handleUnfollow(user._id)}>Unfollow</Button>
+						) : (
+							<Button onClick={() => handleFollow(user._id)}>Follow</Button>
+						)}
 					</List.Item>
 				)}
 			/>
--- src/features/user/userService.js
+++ src/features/user/userService.js
@@ -2,28 +2,58 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/users";
 const getLoggedUser = async () => {
-  const token = JSON.parse(localStorage.getItem("token"));
-  const res = await axios.get(API_URL + "/profile", {
-    headers: {
-      Authorization: token,
-    },
-  });
-  return res.data;
+	const token = JSON.parse(localStorage.getItem("token"));
+	const res = await axios.get(API_URL + "/profile", {
+		headers: {
+			Authorization: token,
+		},
+	});
+	return res.data;
 };
 
 const getUsersByName = async (name) => {
-  const token = JSON.parse(localStorage.getItem("token"));
-  const res = await axios.get(API_URL + "/name/" + name, {
-    headers: {
-      Authorization: token,
-    },
-  });
-  return res.data;
+	const token = JSON.parse(localStorage.getItem("token"));
+	const res = await axios.get(API_URL + "/name/" + name, {
+		headers: {
+			Authorization: token,
+		},
+	});
+	return res.data;
+};
+
+const follow = async (_id) => {
+	const token = JSON.parse(localStorage.getItem("token"));
+	const res = await axios.put(
+		API_URL + "/follow/" + _id,
+		{},
+		{
+			headers: {
+				Authorization: token,
+			},
+		}
+	);
+	return res.data;
+};
+
+const unfollow = async (_id) => {
+	const token = JSON.parse(localStorage.getItem("token"));
+	const res = await axios.put(
+		API_URL + "/unfollow/" + _id,
+		{},
+		{
+			headers: {
+				Authorization: token,
+			},
+		}
+	);
+	return res.data;
 };
 
 const userService = {
-  getLoggedUser,
-  getUsersByName,
+	getLoggedUser,
+	getUsersByName,
+	follow,
+	unfollow,
 };
 
 export default userService;
