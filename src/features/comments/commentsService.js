06741ab8b36bab2a6ad1fe3b6d1647c424cda59b
VeroPolegre <veronicapolegre98@gmail.com>
2023-12-14 11:55:48 +0100

    cleaner code

--- src/features/comments/commentsService.js
+++ src/features/comments/commentsService.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/comments/";
 
 const createComment = async (formData) => {
-  console.log(formData);
   const token = JSON.parse(localStorage.getItem("token"));
   const res = await axios.post(API_URL + formData.postId, formData.form, {
     headers: {
