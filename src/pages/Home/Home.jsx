a6bb140690ac5fae5f78a6887df938bcfccd1e64
VeroPolegre <veronicapolegre98@gmail.com>
2023-12-15 00:57:08 +0100

    correct naming fix at header component import

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Home.scss";
-import Header from "../../components/Header/header";
+import Header from "../../components/Header/Header";
 import Posts from "../../components/Posts/Posts";
 
 const Home = () => {
   return (
     <React.Fragment>
-      <Header/>
-      <Posts/>
+      <Header />
+      <Posts />
     </React.Fragment>
-  )
+  );
 };
 
 export default Home;
