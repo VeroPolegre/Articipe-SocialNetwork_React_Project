d429aa6c8ed826d67eb7dcaf8cac22897f634c1c
VeroPolegre <veronicapolegre98@gmail.com>
2023-12-15 01:05:30 +0100

    despingue

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,35 +1,35 @@
 import { useState } from "react";
-import Header from "../../components/Header/header";
+import Header from "../../components/Header/Header";
 import PostGrid from "../../components/PostGrid/PostGrid";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import UserGrid from "../../components/UserGrid/UserGrid";
 import { useSelector } from "react-redux";
 
 const Explore = () => {
-	const [searchType, setSearchType] = useState("post");
+  const [searchType, setSearchType] = useState("post");
 
-	const handleSearchTypeChange = (type) => {
-		setSearchType(type);
-	};
-	const posts = useSelector((state) => state.posts.posts);
-	const users = useSelector((state) => state.user.searchedUsers);
+  const handleSearchTypeChange = (type) => {
+    setSearchType(type);
+  };
+  const posts = useSelector((state) => state.posts.posts);
+  const users = useSelector((state) => state.user.searchedUsers);
 
-	return (
-		<>
-			<Header
-				onSearchTypeChange={handleSearchTypeChange}
-				searchType={searchType}
-			/>
-			<SearchBar
-				onSearchTypeChange={handleSearchTypeChange}
-				searchType={searchType}
-			/>
-			<>
-				{searchType === "post" && <PostGrid posts={posts} />}
-				{searchType === "user" && <UserGrid users={users} />}
-			</>
-		</>
-	);
+  return (
+    <>
+      <Header
+        onSearchTypeChange={handleSearchTypeChange}
+        searchType={searchType}
+      />
+      <SearchBar
+        onSearchTypeChange={handleSearchTypeChange}
+        searchType={searchType}
+      />
+      <>
+        {searchType === "post" && <PostGrid posts={posts} />}
+        {searchType === "user" && <UserGrid users={users} />}
+      </>
+    </>
+  );
 };
 
 export default Explore;
